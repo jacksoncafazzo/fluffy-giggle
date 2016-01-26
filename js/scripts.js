6fcef9245fee3c5802183920c9a94f7fc7a0ea1c
Jackson Cafazzo and Simon Temple <simonctemple@live.com>
2016-01-25 16:21:12 -0800

    add working version of to do list

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
-function TaskAdd(task) {
-  this.task = task;
+// function TaskAdd(task) {
+//   this.task = task;
+// }
+
+var toListAction = function(task) {
+  return "<li>" + task + "</li>";
 }
 
-TaskAdd.prototype.toListItem = function() {
-  return this.task;
-}
+$(document).ready(function() {
+  $("form#taskTask").submit(function(event) {
+    debugger;
+
+  var inputtedListItem = $("input#taskInput").val();
+  var taskForList = toListAction(inputtedListItem);
+    $("#finalList").show();
+
+    $("ul#listResults").append(taskForList);
+
+  event.preventDefault();
+
+  });
+
+});
--- specs/specs.js
+++ specs/specs.js
@@ -3,4 +3,8 @@ describe('TaskAdd', function() {
     var testTask = new TaskAdd("blank");
     expect(testTask.task).to.equal("blank");
   });
+  it("will return task as list item", function() {
+    var testListItem = new TaskAdd("hotdog");
+    expect(testListItem.toListItem()).to.equal("<li>hotdog</li>");
+  });
 });
